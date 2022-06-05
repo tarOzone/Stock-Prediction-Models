2e49aec44ee58bb1474465f615cc7722c1ca6236
tarOzone <tarthewongkham0@gmail.com>
2022-06-05 18:27:08 +0700

    update tf code

--- stacking/model.py
+++ stacking/model.py
@@ -1,6 +1,9 @@
 import tensorflow as tf
 import numpy as np
 
+tf = tf.compat.v1
+tf.disable_v2_behavior()
+
 class Model:
     def __init__(self, learning_rate, num_layers, size, size_layer, output_size, forget_bias = 0.1):
         
@@ -9,7 +12,7 @@ class Model:
         rnn_cells = tf.nn.rnn_cell.MultiRNNCell([lstm_cell(size_layer) for _ in range(num_layers)], state_is_tuple = False)
         self.X = tf.placeholder(tf.float32, (None, None, size))
         self.Y = tf.placeholder(tf.float32, (None, output_size))
-        drop = tf.contrib.rnn.DropoutWrapper(rnn_cells, output_keep_prob = forget_bias)
+        drop = tf.nn.rnn_cell.DropoutWrapper(rnn_cells, output_keep_prob = forget_bias)
         self.hidden_layer = tf.placeholder(tf.float32, (None, num_layers * 2 * size_layer))
         self.outputs, self.last_state = tf.nn.dynamic_rnn(drop, self.X, initial_state = self.hidden_layer, dtype = tf.float32)
         rnn_W = tf.Variable(tf.random_normal((size_layer, output_size)))
